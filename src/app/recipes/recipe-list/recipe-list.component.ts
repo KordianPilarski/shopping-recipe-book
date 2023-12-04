5ee2b9011cbed073e22ef441fd671e5bd45b5b4a
Kordian <kordian.pilarski@capgemini.com>
2023-12-04 10:53:32 +0100

    add components schema

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'A description of test recipe',
+      'https://www.southernliving.com/thmb/j-l5onbCWgjwaCjtheJfIUqpP9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220406_SL_Southern-Style-Collard-Greens_Beauty_1605-9df766d6d96a4d00aeb240165d7a0669.jpg'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'A description of test recipe',
+      'https://www.southernliving.com/thmb/j-l5onbCWgjwaCjtheJfIUqpP9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220406_SL_Southern-Style-Collard-Greens_Beauty_1605-9df766d6d96a4d00aeb240165d7a0669.jpg'
+    ),
+  ];
 }
