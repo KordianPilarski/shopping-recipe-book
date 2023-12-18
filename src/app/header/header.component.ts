8a69378ee9c137d2e3f72027f250a633082d1382
Kordian <kordian.pilarski@capgemini.com>
2023-12-18 10:49:54 +0100

    use property and event binding in recipe list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() selectedFeature = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.selectedFeature.emit(feature);
+  }
+
+  constructor() {
+    console.log(this.selectedFeature);
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,23 +7,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  chosenRecipeID = '';
-  @Output() choseRecipeID = new EventEmitter<string>();
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
+      'A test Recipe 1',
       'A description of test recipe',
       'https://www.southernliving.com/thmb/j-l5onbCWgjwaCjtheJfIUqpP9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220406_SL_Southern-Style-Collard-Greens_Beauty_1605-9df766d6d96a4d00aeb240165d7a0669.jpg'
     ),
     new Recipe(
-      'A test Recipe',
+      'A test Recipe 2',
       'A description of test recipe',
       'https://www.southernliving.com/thmb/j-l5onbCWgjwaCjtheJfIUqpP9E=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20220406_SL_Southern-Style-Collard-Greens_Beauty_1605-9df766d6d96a4d00aeb240165d7a0669.jpg'
     ),
   ];
 
-  onRecipeClicked(recipeID: string) {
-    this.choseRecipeID.emit(recipeID);
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 }
